e130f52c870667f85ea386ffd39cc8d29d2be22d
lighthart <dlighthart@mesd.k12.or.us>
2015-01-08 17:27:59 -0800

    Renamed Listener

--- Listeners/RoleListener.php
+++ Listeners/RoleListener.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Mesd\UserBundle\Listeners;
 
 use Symfony\Component\DependencyInjection\ContainerInterface as Container;
@@ -7,9 +6,9 @@ use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 
-use Mesd\UserBundle\Entity\User; //Your USER entity
+use Mesd\UserBundle\Entity\User;
 
-class Roles {
+class RoleListener {
     private $context;
 
     public function __construct(SecurityContext $context) {
@@ -20,12 +19,7 @@ class Roles {
         if($this->context->getToken() && $this->context->getToken()->getUser() instanceof User) {
 
             //Custom logic to see if you need to update the role or not.
-            // No logic, always refresh roles
-
-            $user = $this->context->getToken()->getUser();
-
-            //Create new user token
-            //main == firewall setting
+            $user= $this->context->getToken()->getUser();
             $token = new UsernamePasswordToken($user, $user->getPassword(), 'main', $user->getRoles());
 
             $this->context->setToken($token);
