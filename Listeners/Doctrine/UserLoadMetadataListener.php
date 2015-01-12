6d6162c8ec6a2049c068da4f77add2de3b09de03
david <david@blackfishtech.net>
2015-01-12 09:59:49 -0800

    Don't check groups enabled via refection class, if reflection is null - doctrine bug?

--- Listeners/Doctrine/UserLoadMetadataListener.php
+++ Listeners/Doctrine/UserLoadMetadataListener.php
@@ -33,8 +33,12 @@ class UserLoadMetadataListener
     public function loadClassMetadata(LoadClassMetadataEventArgs $args) {
         //We only care about the user entity
         if ('Mesd\UserBundle\Entity\User' === $args->getClassMetadata()->getName()) {
-            //Set the static property groups enabled to be what is set in the configuration
-            $args->getClassMetadata()->getReflectionClass()->getProperty('groupsEnabled')->setValue($this->groupsEnabled);
+            // Reflection class is null when generating entities - may be fixed in newer doctrine.
+            if (null !== $args->getClassMetadata()->getReflectionClass()) {
+                //Set the static property groups enabled in the configuration
+                var_dump($args->getClassMetadata());exit;
+                $args->getClassMetadata()->getReflectionClass()->getProperty('groupsEnabled')->setValue($this->groupsEnabled);
+            }
         }
     }
 
